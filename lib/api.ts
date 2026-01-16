21cfa77e43ae4f90bbb2b933a3004806cb3b7c4e
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-01-16 16:29:26 +0100

    fix

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://flowfunds-backend-ih3t.onrender.com/api";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL,
